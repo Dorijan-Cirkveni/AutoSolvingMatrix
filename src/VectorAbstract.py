cef77d48d4b5c30d9c3d50ae6264f9223ee96044
Dorijan Cirkveni <dorijan.cirkveni@engineer.com>
2020-08-17 12:49:59 +0200

    Created abstract vector class
    So I can apply multiple vectors

---
+++ src/VectorAbstract.py
@@ -0,0 +1,6 @@
+class VectorAbstract:
+    def SolveWith(self,other):
+        other:VectorAbstract
+        raise NotImplementedError
+    def ConvertToCanon(self):
+        raise NotImplementedError
